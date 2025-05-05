01e471fb0094fe9b301204c58126ffbdbf02d193
ja23cardona1406 <jaimeandrescardonam@gmail.com>
2025-05-05 10:22:47 -0500

    version despliegue

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/tesis-final/',
+  base: process.env.VITE_BASE_URL,
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
